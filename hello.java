1d0e163081f68007c1a418411c947e491eacf9d7
Priyansh Vashistha <priyansh2001vashistha@gmail.com>
2025-08-27 15:00:27 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 class WelcomeMessage {
     public static void main(String[] args) {
         System.out.println("Hello, Priyansh! Welcome");
+        System.out.println("I hope your DEvOps journey will be great");
     }
 }
