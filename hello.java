cbfbd89cd14d07b74934380e854009e750b90e38
Priyansh Vashistha <priyansh2001vashistha@gmail.com>
2025-09-01 14:37:01 +0530

    Update hello.java
    Signed-off-by: Priyansh Vashistha <priyansh2001vashistha@gmail.com>

--- hello.java
+++ hello.java
@@ -2,6 +2,5 @@ class WelcomeMessage {
     public static void main(String[] args) {
         System.out.println("Hello, Priyansh! Welcome");
         System.out.println("I hope your DevOps journey will be great");
-        System.out.println("Best of luck! Daily Learn Something new");
     }
 }
